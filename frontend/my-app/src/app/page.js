a5e3b8f65f702ebab4a40bfd830a54e109e9a541
sheraz8141 <sheraz.ahmad8814@gmail.com>
2026-02-08 22:01:38 +0500

    nextjs-frontend

---
+++ frontend/my-app/src/app/page.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to My Next.js App!</h1>
+      <p>This is a simple homepage running inside Docker.</p>
+    </div>
+  );
+}
