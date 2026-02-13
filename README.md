3f31628a3e8264a6fa8cefadb71722e27824f03d
sheraz8141 <sheraz.ahmad8814@gmail.com>
2026-02-14 01:33:31 +0500

    Readme.md-file-addes

---
+++ README.md
@@ -0,0 +1,20 @@
+# EKS 3-Tier GitOps Architecture with Terraform, CI/CD, and Monitoring
+
+This repository demonstrates a **production-ready architecture** for deploying a **3-tier application** inside an **Amazon EKS Cluster** using **GitOps**, **CI/CD**, **Terraform**, and **Monitoring tools**.
+
+The architecture includes:
+
+- **Infrastructure as Code (IaC)** with Terraform to provision AWS resources.
+- **CI/CD Pipeline** using Jenkins to build Docker images and trigger deployments.
+- **GitOps** with ArgoCD to manage Kubernetes resources.
+- **3-Tier Application**:
+  - Frontend Tier (exposed via Load Balancer)
+  - Backend Tier
+  - Database Tier
+- **Kubernetes Network Policies** to control communication between tiers.
+- **Monitoring Stack** using Prometheus and Grafana.
+
+---
+
+## 🏗 Architecture Overview
+
--- frontend/my-app/src/app/page.js
+++ frontend/my-app/src/app/page.js
@@ -1,9 +1,46 @@
+"use client";
+
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_NODEJS_BACKEND}/api/blogs`)
+      .then(res => res.json())
+      .then(data => setBlogs(data));
+  }, []);
+
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Welcome to My Next.js App!</h1>
-      <p>This is a simple homepage running inside Docker.</p>
-    </div>
+    <main className="min-h-screen bg-gray-100 p-8">
+
+      <h1 className="text-4xl font-bold text-center mb-10">
+        DevOps Blog
+      </h1>
+
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+
+        {blogs.map(blog => (
+          <div key={blog.id}
+            className="bg-white rounded-xl shadow-md p-6"
+          >
+            <h2 className="text-xl font-semibold">
+              {blog.title}
+            </h2>
+
+            <p className="text-gray-600 mt-2">
+              {blog.description}
+            </p>
+
+            <p className="text-sm text-gray-400 mt-4">
+              By {blog.author}
+            </p>
+
+          </div>
+        ))}
+
+      </div>
+
+    </main>
   );
 }
